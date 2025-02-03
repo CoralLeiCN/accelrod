33bd59ba070c013405a7b63cb2ace447ca527cb2
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2025-02-03 20:30:40 +0000

    refactor: move GPU memory functions to device.py for better organization

--- accelrod/benchmark.py
+++ accelrod/benchmark.py
@@ -1,36 +1,7 @@
-from torch.utils import benchmark
-import torch
-import pandas as pd
 import matplotlib.pyplot as plt
-import subprocess
-
-
-def get_gpu_free_memory():
-    result = subprocess.check_output(
-        "nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits",
-        shell=True,
-        encoding="utf-8",
-    )
-    return float(result)
-
-
-# determine device
-def get_device():
-    if torch.cuda.is_available():
-        return "cuda"
-    elif torch.backends.mps.is_available():
-        return "mps"
-    else:
-        return "cpu"
-
-
-def get_device_properties():
-    if torch.cuda.is_available():
-        return torch.cuda.get_device_properties(torch.cuda.device("cuda"))
-    elif torch.backends.mps.is_available():
-        return
-    else:
-        return
+import pandas as pd
+import torch
+from torch.utils import benchmark
 
 
 # get bytes based on the dtype
