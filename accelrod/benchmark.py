2250c835f23f9c7ba2b4bc1506ccf82122edd0e9
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2025-02-06 22:35:22 +0000

    fix: update timer_GEMM to return Timer after run and adjust benchmark_GEMM accordingly

--- accelrod/benchmark.py
+++ accelrod/benchmark.py
@@ -66,7 +66,9 @@ def timer_GEMM(m, k, n, dtype=torch.float32, device=None, number=50) -> benchmar
         stmt=f"d = a @ b + c; torch.{device}.synchronize()",
         globals={"a": a, "b": b, "c": c},
     )
-    return t
+    x = t.timeit(number=number)
+
+    return x
 
 
 def benchmark_GEMM(matrix_shape, dtype=torch.float16, device=None, number=50):
@@ -82,9 +84,8 @@ def benchmark_GEMM(matrix_shape, dtype=torch.float16, device=None, number=50):
     # get bytes based on the dtype
     bytes_per_element = get_bytes_by_dtype(dtype)
     print(f"dtype is {dtype}, bytes_per_element: {bytes_per_element}")
-    t = timer_GEMM(m=m, k=k, n=n, dtype=dtype, device=device, number=number)
+    x = timer_GEMM(m=m, k=k, n=n, dtype=dtype, device=device, number=number)
 
-    x = t.timeit(number=number)
     number_FLOPS = 2 * m * n * k + m * n
     number_bytes_accesses = bytes_per_element * (
         m * k + k * n + 2 * m * n
